d25625b66026d22662bf322c91ae5b1b25e51e0e
Andriy <sadhjsa@gmail.com>
2024-06-23 13:15:24 +0300

    add walk and circles

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from random import randint
+import math
+
 
 WIDTH = 1000
 HEIGHT = 600
@@ -9,8 +11,8 @@ FPS = 60
 platform_num = 3
 circle_num = 5
 
-circles = []
-platformes = []
+circles = pygame.sprite.Group()
+platformes = pygame.sprite.Group()
 
 window = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
@@ -19,6 +21,7 @@ background = pygame.transform.scale(pygame.image.load("background.jpg"), SIZE)
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, filename, size, coords):
+        super().__init__()
         self.image = pygame.image.load(filename)
         self.image = pygame.transform.scale(pygame.image.load(filename), size)
         self.rect = self.image.get_rect()
@@ -29,11 +32,39 @@ class GameSprite(pygame.sprite.Sprite):
         window.blit(self.image, self.rect)
 
 class Player(GameSprite):
-    def update(self):
+    def __init__(self, filename, size, coords):
+        super().__init__(filename, size, coords)
+        self.angle = math.pi / 4
+        self.speed = 0
+        self.acceleration = 0
+        self.attached = False
+    def update(self, platforms, circles):
         keys = pygame.key.get_pressed()
+        if self.attached:
+            self.acceleration = 1
+            self.speed += self.acceleration
+        else:
+            if keys[pygame.K_d]:
+                self.rect.x += 5
+            elif keys[pygame.K_a]:
+                self.rect.x -= 5
+            elif keys[pygame.K_w]:
+                self.rect.y -= 8
+            if not self.collide_platform(platforms):
+                self.rect.y += 4
+            collided_circles = pygame.sprite.spritecollide(self, circles, False)
+            if collided_circles:
+                self.attached = True
+                point = collided_circles[0].rect.center
+                
 
-        if keys [pygame.K_w]:
-            self.rect.y -= 5
+    def collide_platform(self, platforms):
+        collided_platforms = pygame.sprite.spritecollide(self, platforms, False)
+        for c in collided_platforms:
+            if self.rect.bottom > c.rect.top:
+                return True
+        return False
+        
 
 
 
@@ -46,18 +77,24 @@ class Player(GameSprite):
 
 
 
-circle = GameSprite("circle.png", (50, 50), (400, 150))
-platform = GameSprite("square.png", (200, 35), (600, 300))
+
+
+
+
 item = GameSprite("square.png", (500,400), (0, 600))
-stickman = GameSprite("stickman.png", (100, 100), (100, 400))
+stickman = Player("stickman.png", (100, 100), (100,300))
 
-for i in range(circle_num):
-    new_circle = GameSprite("circle.png", (50, 75), (randint(100, 1200), randint(50, 50)))
-    circles.append(new_circle)
-
+circles.add(
+    GameSprite("circle.png", (50, 75), (300, 50)),
+    GameSprite("circle.png", (50, 75), (600, 50)),
+    GameSprite("circle.png", (50, 75), (900, 50))
+)
 for i in range(platform_num):
-    new_platform = GameSprite("square.png", (50, 75), (randint(600, 1200), randint(200, 900)))
-    platformes.append(new_platform)
+    new_platform = GameSprite("square.png", (50, 75), (randint(WIDTH//2, WIDTH), randint(200, HEIGHT-200)))
+    platformes.add(new_platform)
+
+platformes.add(item)
+#!platformes.add(GameSprite("square.png", (50,75), (100, 500)))    
 
 game_over = False
 while not game_over:
@@ -65,12 +102,9 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
     window.blit(background, (0, 0))
+    stickman.update(platformes, circles)
     stickman.reset()
-    item.reset()
-    platform.reset()
-    for c in circles:
-        c.reset()
-    for t in platformes:
-        t.reset()
+    circles.draw(window)
+    platformes.draw(window)
     pygame.display.update()
     clock.tick(FPS)
